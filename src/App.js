9f77a80c2c516b8939169457e9d1503bdf6d5a97
Noble Okafor <okafornoble202@gmail.com>
2022-08-19 15:34:10 +0100

    finish nft contract

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { Container, Nav } from "react-bootstrap";
 import { useContractKit } from "@celo-tools/use-contractkit";
 import { Notification } from "./components/ui/Notifications";
 import Wallet from "./components/Wallet";
-import Cover from "./components/Cover";
-import Counter from "./components/Counter";
-import { useBalance, useCounterContract } from "./hooks";
+import Cover from "./components/minter/Cover";
+import Nfts from "./components/minter/nfts";
+import { useBalance, useMinterContract } from "./hooks";
+import coverImg from "./assets/img/nft_geo_cover.png";
 import "./App.css";
-
 const App = function AppWrapper() {
   const { address, destroy, connect } = useContractKit();
-  const { balance } = useBalance();
-  const counterContract = useCounterContract();
-
+  const { balance, getBalance } = useBalance();
+  const minterContract = useMinterContract();
   return (
     <>
       <Notification />
@@ -20,7 +19,6 @@ const App = function AppWrapper() {
         <Container fluid="md">
           <Nav className="justify-content-end pt-3 pb-5">
             <Nav.Item>
-              {/*display user wallet*/}
               <Wallet
                 address={address}
                 amount={balance.CELO}
@@ -29,18 +27,16 @@ const App = function AppWrapper() {
               />
             </Nav.Item>
           </Nav>
-          {/* display cover */}
           <main>
-            <Counter counterContract={counterContract} />
+            <Nfts
+              name="GEO Collection"
+              updateBalance={getBalance}
+              minterContract={minterContract}
+            />
           </main>
         </Container>
       ) : (
-        // display cover if user is not connected
-        <div className="App">
-          <header className="App-header">
-            <Cover connect={connect} />
-          </header>
-        </div>
+        <Cover name="GEO Collection" coverImg={coverImg} connect={connect} />
       )}
     </>
   );
